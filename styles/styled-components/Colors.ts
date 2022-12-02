fea07ce8da6baea2e0c5a5260f2e52dd6585f481
Ikarosv <ikarovieiraa@gmail.com>
2022-12-02 15:23:38 -0300

    refactor: consertando erro do rext dark accent

--- styles/styled-components/Colors.ts
+++ styles/styled-components/Colors.ts
@@ -1,6 +1,6 @@
 // DARK MODE
 export const TEXT_DARK = '#391400'
-export const TEXT_DARK_ACCENT = '##EF6D58';
+export const TEXT_DARK_ACCENT = '#EF6D58';
 export const TEXT_DARK_GRAY = 'rgba(57, 20, 0, 0.64)'
 
 // LIGHT MODE
