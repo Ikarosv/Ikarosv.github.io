cb86af1e6188c43d4e8ccb28c53d1d2d37a0e6da
Ikarosv <ikarovieiraa@gmail.com>
2022-12-02 15:07:40 -0300

    style: adicioando export default

--- styles/styled-components/Colors.ts
+++ styles/styled-components/Colors.ts
@@ -7,3 +7,14 @@ export const TEXT_DARK_GRAY = 'rgba(57, 20, 0, 0.64)'
 export const TEXT_LIGHT = '#FFFFFF';
 export const TEXT_LIGHT_ACCENT = '#EF6D58';
 export const TEXT_LIGHT_GRAY = 'rgba(255, 255, 255, 0.64)'
+
+const Colors = {
+  TEXT_DARK,
+  TEXT_DARK_ACCENT,
+  TEXT_DARK_GRAY,
+  TEXT_LIGHT,
+  TEXT_LIGHT_ACCENT,
+  TEXT_LIGHT_GRAY,
+}
+
+export default Colors;
