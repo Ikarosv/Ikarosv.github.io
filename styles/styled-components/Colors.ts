ae5ff50bb4eff23921f754403992f715d7242f92
Ikarosv <ikarovieiraa@gmail.com>
2022-12-02 15:04:38 -0300

    style: Criada cores padrão para textos

---
+++ styles/styled-components/Colors.ts
@@ -0,0 +1,9 @@
+// DARK MODE
+export const TEXT_DARK = '#391400'
+export const TEXT_DARK_ACCENT = '##EF6D58';
+export const TEXT_DARK_GRAY = 'rgba(57, 20, 0, 0.64)'
+
+// LIGHT MODE
+export const TEXT_LIGHT = '#FFFFFF';
+export const TEXT_LIGHT_ACCENT = '#EF6D58';
+export const TEXT_LIGHT_GRAY = 'rgba(255, 255, 255, 0.64)'
