ab2fc27f494defecb829f86b7e34fc9ebded7add
Ikarosv <ikarovieiraa@gmail.com>
2023-01-18 13:34:10 -0300

    feat: adiciona cores
    Adiconei novas cores e removi outras, mudei também o objeto text colors para referenciar aos temas

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -1,44 +1,29 @@
 // DARK MODE
-export const DARK: string = '#391400';
-export const DARK_ACCENT: string = '#EF6D58';
-export const DARK_GRAY: string = 'rgba(57, 20, 0, 0.64)'
+export const DARK: string = '#111111';
+export const DARK_GRAY: string = '#2C2C2C'
 
 // LIGHT MODE
 export const LIGHT: string = '#FFFFFF';
-export const LIGHT_ACCENT: string = '#EF6D58';
-export const LIGHT_GRAY: string = 'rgba(255, 255, 255, 0.64)';
+export const LIGHT_GRAY: string = '#EBEBEB';
 
+
+export const ACCENT: string = '#EF6D58';
 export const PURPLE: string = '#B550F8';
+export const YELLOW: string = '#F9A826'
 
 export const TextColors = {
-  dark: {
-    color: DARK,
-  },
-  dark_accent: {
-    color: DARK_ACCENT,
-  },
-  dark_gray: {
-    color: DARK_GRAY,
-  },
-  light: {
-    color: LIGHT,
-  },
-  light_accent: {
-    color: LIGHT_ACCENT,
-  },
-  light_gray: {
-    color: LIGHT_GRAY,
-  }
+  lightMode: DARK_GRAY,
+  darkMode:  LIGHT_GRAY,
 };
 
 const Colors = {
   DARK,
-  DARK_ACCENT,
   DARK_GRAY,
   LIGHT,
-  LIGHT_ACCENT,
   LIGHT_GRAY,
   PURPLE,
+  ACCENT,
+  YELLOW,
 }
 
 export default Colors;
