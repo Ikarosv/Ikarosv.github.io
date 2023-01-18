f3d37e530c82c05cb9c3cd12152b3db2b44f6e23
Ikarosv <ikarovieiraa@gmail.com>
2023-01-18 19:48:55 -0300

    refactor: mudado estilo do header
    Mudei o estilo do header, mudei cor, texto e adicionei botão de dark e light mode.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,29 +1,22 @@
-import { styled } from "../../stitches.config";
-import { TextColors } from "../../styles/Colors";
-import Button from "../../styles/styled-components/Button";
+import * as Switch from "@radix-ui/react-switch";
+import { MoonSvg, SunSvg } from "../../icons";
+import { styled, theme } from "../../stitches.config";
 import Text from "../../styles/styled-components/Text";
-import Title from "../../styles/styled-components/Title";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 
 export const StyledHeader = styled('header', {
-  backgroundColor: '$bgDark',
-  color: '$LIGHT',
+  height: '113px',
+  maxHeight: '113px',
+  backgroundColor: '$section',
+  color: '$textColor',
   padding: '16px $responsivePadding',
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   '@bp2': {
     padding: '$padding'
-  }
+  },
 });
-
-export const Colored = styled('span', {
-  variants: {
-    color: {
-      ...TextColors
-    }
-  }
-})
 
 const Nav = styled('nav', {
   display: 'flex',
@@ -33,19 +26,51 @@ const Nav = styled('nav', {
   }
 })
 
-export default function Header() {
+const SwitchRoot = styled(Switch.Root, {
+  all: 'unset',
+  width: 42,
+  height: 25,
+  backgroundColor: '#C4C4C4',
+  borderRadius: '9999px',
+  position: 'relative',
+  boxShadow: `0 2px 10px $blackA7`,
+  WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+  '&:focus': { boxShadow: `0 0 0 2px black` },
+  '@bp2': {
+    marginLeft: '22px'
+  }
+});
+
+const SwitchThumb = styled(Switch.Thumb, {
+  display: 'block',
+  width: 23,
+  height: 23,
+  backgroundColor: '$DARK',
+  borderRadius: '9999px',
+  boxShadow: '0 2px 2px $blackA7',
+  transition: 'transform 100ms',
+  transform: 'translateX(2px)',
+  willChange: 'transform',
+  '&[data-state="checked"]': { transform: 'translateX(19px)' },
+});
+
+export default function Header({  isDarkMode, setIsDarkMode }: { isDarkMode: boolean, setIsDarkMode: Function}) {
+
   return (
     <StyledHeader>
-      <Title headNumber={4}>
-        Ikaro<Colored color="dark_accent">Vieira</Colored>
-      </Title>
+      <SwitchRoot onCheckedChange={ () => { setIsDarkMode(!isDarkMode) } }>
+        <SwitchThumb>
+          { !isDarkMode ? <MoonSvg /> : <SunSvg /> }
+        </SwitchThumb>
+      </SwitchRoot>
       <HamburgerMenu />
       <Nav>
-        <Text as="a" href="#" color="light">Sobre</Text>
-        <Text as="a" href="#" color="light">Tecnologias</Text>
-        <Text as="a" href="#" color="light">Projetos</Text>
+        <Text as="a" href="#Sobre">Sobre Mim</Text>
+        <Text as="a" href="#Projetos">Projetos</Text>
+        <Text as="a" href="#Skills">Skills</Text>
+        <Text as="a" href="#Educacao">Educação</Text>
       </Nav>
-      <Button secondary as="a" href="mailto:ikarovieiraa@gmail.com" css={{'@bp2': {display: 'none'}}}>Contato</Button>
+      {/* <Button secondary as="a" href="mailto:ikarovieiraa@gmail.com" css={{'@bp2': {display: 'none'}}}>Contato</Button> */}
     </StyledHeader>
   );
 }
