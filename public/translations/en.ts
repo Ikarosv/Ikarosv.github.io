69fa76a63f0c1113926c6dcb1d3d8cd8d7b89cbc
Ikarosv <ikarovieiraa@gmail.com>
2023-07-01 04:26:22 -0300

    🦄 refactor: adicionado novas frases

--- public/translations/en.ts
+++ public/translations/en.ts
@@ -3,6 +3,12 @@ const en = {
   projects: 'projects',
   about_me: 'about_me',
   contact: 'contact',
+  homeTitle:
+    'Ikaro is a <span class="text-primary">full-stack web developer</span>.',
+  homeDescription:
+    'He loves learning, solving problems, and is passionate about technology and creativity.',
+  working: 'Currently working on ',
+  contactMe: 'Contact me',
 }
 
 export default en
--- public/translations/pt.ts
+++ public/translations/pt.ts
@@ -3,6 +3,12 @@ const pt = {
   projects: 'projetos',
   about_me: 'sobre_mim',
   contact: 'contato',
+  homeTitle:
+    'Ikaro é um <span class="text-primary">desenvolvedor web full-stack</span>.',
+  homeDescription:
+    'Ele ama aprender, resolver problemas e é apaixonado por tecnologia e criatividade.',
+  working: 'Atualmente trabalhando em ',
+  contactMe: 'Contate-me',
 }
 
 export default pt
