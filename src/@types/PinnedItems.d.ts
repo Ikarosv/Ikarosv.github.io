21cfc0e74d772f705c2d70dca04a04ff12cb49d9
Ikaro Vieira <ikarovieiraa@gmail.com>
2024-01-03 16:55:00 +0000

    🏷️ type: tipagem do PinnedItems

---
+++ src/@types/PinnedItems.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+declare namespace GithubRepository {
+  interface PinnedItems {
+    name: string
+    projectsUrl: string
+    homepageUrl: string
+    url: string
+    repositoryTopics: string[]
+  }
+}
