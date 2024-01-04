b1e8c92746231f50c0b28331117c42aeb5a40c01
Ikaro Vieira <ikarovieiraa@gmail.com>
2024-01-04 14:53:34 +0000

    ✨ feat: adiciona projetos pinados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable no-undef */
 import Home from '@/components/Home'
 import { cookies } from 'next/headers'
 import translations, { Lang } from '../../public/translations'
 import Image from 'next/image'
 import Title from '@/components/Title'
 import Link from 'next/link'
+import Card from '@/components/Card'
 
-export default function Homee() {
+const url = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+
+export default async function Homee() {
   const lang: Lang = (cookies().get('LANG')?.value as Lang) ?? 'pt'
+  const githubProjects = await fetch(`${url}/api/github`).then<
+    GithubRepository.PinnedItems[]
+  >((res) => res.json())
 
   const { quote, projects, viewAll } = translations[lang]
   return (
@@ -33,14 +40,32 @@ export default function Homee() {
         </figcaption>
       </figure>
       <span className="w-[5.6875rem] h-[5.6875rem] hidden border border-solid border-gray absolute top-[115vh] -right-2 md:block" />
-      <section className="flex justify-between items-center w-full">
-        <Title lineWidth="max-w-[31rem]">{projects}</Title>
-        <Link
-          href="/projects"
-          className="text-base p-1 font-medium text-white cursor-pointer hover:text-gray min-w-fit ml-4"
-        >
-          {viewAll}
-        </Link>
+      <section>
+        <div className="flex max-ssm:flex-wrap justify-between items-center w-full">
+          <Title lineWidth="max-w-[31rem]">{projects}</Title>
+          <Link
+            href="/projects"
+            className="text-base p-1 font-medium text-white cursor-pointer hover:text-gray min-w-fit ml-4"
+          >
+            {viewAll}
+          </Link>
+        </div>
+        <div className="flex flex-wrap gap-4 mt-12 justify-center">
+          {githubProjects.map((project) => (
+            <Card key={project.id}>
+              <Card.Image />
+              <Card.Topics topics={project.repositoryTopics} />
+              <Card.Details
+                title={project.name}
+                description={
+                  project.description || "I don't have a description"
+                }
+                projectUrl={project.url}
+                homepageUrl={project.homepageUrl}
+              />
+            </Card>
+          ))}
+        </div>
       </section>
     </main>
   )
