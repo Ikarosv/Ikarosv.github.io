792c8fc6846e44b7221a2f1c2a1bea67f264ecd1
Ikaro Vieira <ikarovieiraa@gmail.com>
2024-10-22 22:42:01 -0300

    feat/ -> Atualiza desc e title

--- public/translations/en.ts
+++ public/translations/en.ts
@@ -4,9 +4,9 @@ const en = {
   aboutMe: 'about_me',
   contact: 'contact',
   homeTitle:
-    'Ikaro is a <span class="text-primary">full-stack web developer</span>.',
+    'I am a <span class="text-primary">full-stack web developer</span>.',
   homeDescription:
-    'He loves learning, solving problems, and is passionate about technology and creativity.',
+    'I love learning, solving problems and I am passionate about technology and creativity.',
   working: 'Currently working on ',
   contactMe: 'Contact me !!',
   quote: 'With great power comes great electricity bill',
